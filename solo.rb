f6a4b41c1af6ff84b9c1206a4f1507c6c52bbcc4
FGtatsuro <fujiistorage@gmail.com>
2013-04-08 12:13:26 +0900

    Add bare chef repository.

---
+++ solo.rb
@@ -0,0 +1,6 @@
+file_cache_path           "/Users/tatsuro/.chef/cache"
+data_bag_path             "/Users/tatsuro/repos/MyChefRepo/data_bags"
+encrypted_data_bag_secret "/Users/tatsuro/repos/MyChefRepo/data_bag_key"
+cookbook_path             ["/Users/tatsuro/repos/MyChefRepo/cookbooks", 
+                           "/Users/tatsuro/repos/MyChefRepo/site-cookbooks"]
+role_path                 "/Users/tatsuro/repos/MyChefRepo/roles"
