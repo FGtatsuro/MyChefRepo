932f42040420dc953a4e147156f61d23e348d034
FGtatsuro <fujiistorage@gmail.com>
2013-04-10 01:53:20 +0900

    Use Berkshelf.

--- solo.rb
+++ solo.rb
@@ -2,5 +2,6 @@ file_cache_path           "/Users/tatsuro/.chef/cache"
 data_bag_path             "/Users/tatsuro/repos/MyChefRepo/data_bags"
 encrypted_data_bag_secret "/Users/tatsuro/repos/MyChefRepo/data_bag_key"
 cookbook_path             ["/Users/tatsuro/repos/MyChefRepo/cookbooks", 
-                           "/Users/tatsuro/repos/MyChefRepo/site-cookbooks"]
+                           "/Users/tatsuro/repos/MyChefRepo/site-cookbooks",
+                           "/Users/tatsuro/repos/MyChefRepo/vendor/cookbooks"]
 role_path                 "/Users/tatsuro/repos/MyChefRepo/roles"
