f1270c4b624bf1f4fe52f01f594fc4ca33fd354d
FGtatsuro <fujiistorage@gmail.com>
2013-04-11 01:33:38 +0900

    Add cdh cookbook.

---
+++ cookbooks/cdh/recipes/default.rb
@@ -0,0 +1,46 @@
+#
+# Cookbook Name:: cdh
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+cloudera_repo_base = "http://archive.cloudera.com/cdh4"
+gpg_key_package = "RPM-GPG-KEY-cloudera"
+cdh_package = "cloudera-cdh-4-0.x86_64.rpm"
+
+remote_file "/tmp/#{gpg_key_package}" do
+  source "#{cloudera_repo_base}/redhat/6/x86_64/cdh/#{gpg_key_package}"
+  action :create_if_missing
+end
+
+rpm_package "gpg_key_package" do
+  action :install
+  options "--import"
+  source "/tmp/#{gpg_key_package}"
+end
+
+remote_file  "/tmp/#{cdh_package}" do
+  source "#{cloudera_repo_base}/one-click-install/redhat/6/x86_64/#{cdh_package}"
+  action :create_if_missing
+end
+
+rpm_package "cdh_package" do
+  action :install
+  options "-ivh"
+  source "/tmp/#{cdh_package}"
+end
+
+yum_package "hadoop-0.20-conf-pseudo" do
+  action :install
+end
+
+execute 'namenode_format' do
+  command 'hdfs namenode -format'
+  user 'hdfs'
+  group 'hdfs'
+  not_if {File.exists?('/var/lib/hadoop-hdfs/cache/hdfs/dfs/name')}
+end
