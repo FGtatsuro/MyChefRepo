b2544b2933cb948db40ea93b5a70aadbd620b199
FGtatsuro <fujiistorage@gmail.com>
2013-04-13 01:15:34 +0900

    Add mapreduce cookbook.

---
+++ cookbooks/mapreduce/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: mapreduce
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+service "hadoop-0.20-mapreduce-jobtracker" do
+  action :start
+  notifies :start, "service[hadoop-0.20-mapreduce-tasktracker]", :immediately
+end
+
+service "hadoop-0.20-mapreduce-tasktracker" do
+  action :start
+end
