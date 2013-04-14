96dd4310e91ae614b4d5f5b32b0d10f388f1757c
FGtatsuro <fujiistorage@gmail.com>
2013-04-15 01:09:26 +0900

    Add hostname command.

--- cookbooks/os_base/recipes/default.rb
+++ cookbooks/os_base/recipes/default.rb
@@ -7,6 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
+execute "hostname" do
+  command "hostname hadoopenv"
+  user "root"
+end
+
 yum_package "yum-plugin-fastestmirror" do
   action :install
   options "-y"
